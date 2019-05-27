1ffc38cc69a3ff3ffc9745c07425f5c7165abf8c
yangzhandong <yang1986jl@163.com>
2019-05-27 09:21:06 +0800

    first commit

---
+++ learn-lession/src/main/java/com/nosoft/eut/learnlession/HelloWord.java
@@ -0,0 +1,30 @@
+package com.nosoft.eut.learnlession;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+import sun.misc.Request;
+
+import javax.xml.ws.Response;
+
+@RestController
+@EnableAutoConfiguration
+public class HelloWord {
+
+    @RequestMapping("/")
+    public String home() {
+        System.out.println("哈哈哈哈哈哈哈哈");
+        return "hello  ni ya gaga !";
+    }
+        public static void main(String[] args){
+            SpringApplication.run(HelloWord.class,args);
+        }
+
+    @RequestMapping("/test")
+    public void ModelAndView(Request request, Response response){
+        ModelAndView view  = new ModelAndView();
+
+    }
+}
---
+++ learn-lession/src/main/java/com/nosoft/eut/learnlession/LearnLessionApplication.java
@@ -0,0 +1,13 @@
+package com.nosoft.eut.learnlession;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnLessionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnLessionApplication.class, args);
+	}
+
+}
